602c32bd464109352e84659b9101e4705ae0cd60
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-20 22:23:26 +0800

    Update SIM800L_test.ino

--- SIM800L_test/SIM800L_test.ino
+++ SIM800L_test/SIM800L_test.ino
@@ -1,39 +1,70 @@
-#include <SoftwareSerial.h>
-SoftwareSerial sim(10, 11);
-int _timeout;
-String _buffer;
-String number = "+639770798025";
+//How to use SIM800L  with arduion
+//PS works 
 
-void setup() {
-  // put your setup code here, to run once:
-//delay(7000);
-Serial.begin(9600);
-_buffer.reserve(50);
-Serial.println("System Started...");
-sim.begin(9600);
-delay(1000);
-Serial.println("Type s to send an SMS, r to receive an SMS, and c to make a call.")
+// COnnect the RX TX to Pin 10,11 on Arduion
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10,11);
+int timeout;
+void setup(){
+  Serial.begin(9600); 
+  mySerial.begin(9600);
+  Serial.println("Type\n s) to send an SMS\n r) to receive an SMS\n c) to make a call");
+  Serial.println("System Initializing..."); 
+  delay(1000);
+  mySerial.println("AT");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-if (Serial.available() > 0)
-  switch (Serial.read())
-  {
-    case 's':
-      SendMessage();
-      break;
-    case 'r':
-      ReceiveMessage();
-      break;
-    case 'c';
-      callNumber();
-      break;
+void loop(){
+  if (Serial.available() > 0){
+    switch (Serial.read()){
+      case 's':
+        SendMessage();
+        break;
+      case 'r':
+        RecieveMessage();
+        break;
+      case 'c':
+        CallNumber();
+        break;
+    }
+ }
+  if (mySerial.available()) {
+    delay(1000);
+    Serial.println(mySerial.readString());
   }
-if (sim.available() > 0)
-  Serial.write(sim.read());
-  {
-    void SendMessage()
+}
+
+String readSerial(){
+  //delay(100);
+  if (mySerial.available()) {
+    return mySerial.readString();
   }
-  sim
+}
+
+void CallNumber() {
+  mySerial.println("ATD+ +9568517289;");   //Mobile phone number to call
+  Serial.println(readSerial());
+  delay(20000); 
+  mySerial.println("ATH"); 
+  delay(200);
+  Serial.println(readSerial());
+}
+void SendMessage(){
+  mySerial.println("AT+CMGF=1");     //Sets the GSM Module in Text Mode
+  Serial.println(readSerial());
+  mySerial.println("AT+CMGS=\"+9568517289\"");    //Mobile phone number to send message
+  Serial.println(readSerial());
+  mySerial.println("Hi, This Test Msg From SIM800");
+  mySerial.println((char)26);
+  Serial.println(readSerial());
+}
+
+void RecieveMessage(){
+  Serial.println ("SIM800  Read an SMS");
+  mySerial.println("AT+CMGF=1");    // AT Command to receive a live SMS
+  Serial.println(readSerial());
+  mySerial.println("AT+CNMI=2,1,0,0,0"); 
+  Serial.println(readSerial());
 }
