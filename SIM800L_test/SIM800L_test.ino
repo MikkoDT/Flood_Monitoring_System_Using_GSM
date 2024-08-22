bb4ae10b359e4292441a47f8031ac009a96dc110
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-22 17:16:48 +0800

    Update SIM800L_test.ino

--- SIM800L_test/SIM800L_test.ino
+++ SIM800L_test/SIM800L_test.ino
@@ -1,70 +1,28 @@
-//How to use SIM800L  with arduion
-//PS works 
-
-// COnnect the RX TX to Pin 10,11 on Arduion
-
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(0,1);
-int timeout;
-void setup(){
-  Serial.begin(9600); 
-  mySerial.begin(9600);
-  Serial.println("Type\n s) to send an SMS\n r) to receive an SMS\n c) to make a call");
-  Serial.println("System Initializing..."); 
-  delay(1000);
-  mySerial.println("AT");
+SoftwareSerial sim800l(2, 3); // RX, TX
+
+void setup() {
+  Serial.begin(9600);
+  sim800l.begin(9600);
 }
 
-void loop(){
-  if (Serial.available() > 0){
-    switch (Serial.read()){
-      case 's':
-        SendMessage();
-        break;
-      case 'r':
-        RecieveMessage();
-        break;
-      case 'c':
-        CallNumber();
-        break;
+void loop() {
+  if (Serial.available() > 0) {
+    String input = Serial.readStringUntil('\n');
+    if (input == "sms") {
+      sendSMS();
     }
- }
-  if (mySerial.available()) {
-    delay(1000);
-    Serial.println(mySerial.readString());
   }
 }
 
-String readSerial(){
+void sendSMS() {
+  sim800l.println("AT+CMGF=1"); // Set SMS mode to text
   delay(100);
-  if (mySerial.available()) {
-    return mySerial.readString();
-  }
-}
-
-void CallNumber() {
-  mySerial.println("ATD+ +639568517289;");   //Mobile phone number to call
-  Serial.println(readSerial());
-  delay(20000); 
-  mySerial.println("ATH"); 
-  delay(200);
-  Serial.println(readSerial());
-}
-void SendMessage(){
-  mySerial.println("AT+CMGF=1");     //Sets the GSM Module in Text Mode
-  Serial.println(readSerial());
-  mySerial.println("AT+CMGS=\"+639568517289\"");    //Mobile phone number to send message
-  Serial.println(readSerial());
-  mySerial.println("Hi, This Test Msg From SIM800");
-  mySerial.println((char)26);
-  Serial.println(readSerial());
-}
-
-void RecieveMessage(){
-  Serial.println ("SIM800  Read an SMS");
-  mySerial.println("AT+CMGF=1");    // AT Command to receive a live SMS
-  Serial.println(readSerial());
-  mySerial.println("AT+CNMI=1,2,0,0,0"); 
-  Serial.println(readSerial());
+  sim800l.println("AT+CMGS=\"+639568517289\""); // Replace with the number you want to send the SMS to
+  delay(100);
+  sim800l.print("Hello, this is a test message."); // Replace with the message you want to send
+  delay(100);
+  sim800l.write(26); // End the message by sending the ASCII code of Ctrl+Z
+  delay(100);
 }
