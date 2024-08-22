aa8b9daa201175bdb73fc9ee816f4e7232ebdfc9
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-22 20:50:50 +0800

    Update SIM800L_test.ino

--- SIM800L_test/SIM800L_test.ino
+++ SIM800L_test/SIM800L_test.ino
@@ -1,28 +1,73 @@
 #include <SoftwareSerial.h>
-
-SoftwareSerial sim800l(2, 3); // RX, TX
-
+SoftwareSerial sim(0, 1);
+int _timeout;
+String _buffer;
+String number = "+639770798025"; //-> change with your number
 void setup() {
+  //delay(7000); //delay for 7 seconds to make sure the modules get the signal
   Serial.begin(9600);
-  sim800l.begin(9600);
+  _buffer.reserve(50);
+  Serial.println("Sistem Started...");
+  sim.begin(9600);
+  delay(1000);
+  Serial.println("Type s to send an SMS, r to receive an SMS, and c to make a call");
 }
-
 void loop() {
-  if (Serial.available() > 0) {
-    String input = Serial.readStringUntil('\n');
-    if (input == "sms") {
-      sendSMS();
+  if (Serial.available() > 0)
+    switch (Serial.read())
+    {
+      case 's':
+        SendMessage();
+        break;
+      case 'r':
+        RecieveMessage();
+        break;
+      case 'c':
+        callNumber();
+        break;
     }
+  if (sim.available() > 0)
+    Serial.write(sim.read());
+}
+void SendMessage()
+{
+  //Serial.println ("Sending Message");
+  sim.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
+  delay(200);
+  //Serial.println ("Set SMS Number");
+  sim.println("AT+CMGS=\"" + number + "\"\r"); //Mobile phone number to send message
+  delay(200);
+  String SMS = "Hello, how are you? greetings from miliohm.com admin";
+  sim.println(SMS);
+  delay(100);
+  sim.println((char)26);// ASCII code of CTRL+Z
+  delay(200);
+  _buffer = _readSerial();
+}
+void RecieveMessage()
+{
+  Serial.println ("SIM800L Read an SMS");
+  sim.println("AT+CMGF=1");
+  delay (200);
+  sim.println("AT+CNMI=1,2,0,0,0"); // AT Command to receive a live SMS
+  delay(200);
+  Serial.write ("Unread Message done");
+}
+String _readSerial() {
+  _timeout = 0;
+  while  (!sim.available() && _timeout < 12000  )
+  {
+    delay(13);
+    _timeout++;
+  }
+  if (sim.available()) {
+    return sim.readString();
   }
 }
-
-void sendSMS() {
-  sim800l.println("AT+CMGF=1"); // Set SMS mode to text
-  delay(100);
-  sim800l.println("AT+CMGS=\"+639568517289\""); // Replace with the number you want to send the SMS to
-  delay(100);
-  sim800l.print("Hello, this is a test message."); // Replace with the message you want to send
-  delay(100);
-  sim800l.write(26); // End the message by sending the ASCII code of Ctrl+Z
-  delay(100);
+void callNumber() {
+  sim.print (F("ATD"));
+  sim.print (number);
+  sim.print (F(";\r\n"));
+  _buffer = _readSerial();
+  Serial.println(_buffer);
 }
