3f256d47e32b93599713576e04a8f9c61a540a5b
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-28 19:32:24 +0800

    update

--- float_test/float_test.ino
+++ float_test/float_test.ino
@@ -1,5 +1,31 @@
 //Set the LED light to pin 8
+12345678910111213
+//Set the LED light to pin 8
 int LED = 8;
+
+12345678910111213
+//Set the LED light to pin 8
+int LED = 8;
+
+//Set the float sensor to pin 3
+#define Float_Switch 3
+
+
+
+int LED = 8;
+
+12345678910111213
+//Set the LED light to pin 8
+int LED = 8;
+
+//Set the float sensor to pin 3
+#define Float_Switch 3
+
+void setup() {
+
+  // initialize digital pin 8 as an output.
+  pinMode(LED, OUTPUT);
+
 
 //Set the float sensor to pin 3
 #define Float_Switch 3
